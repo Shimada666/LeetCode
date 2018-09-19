fda2443822843878e2724bf32ea4884cf6b3da52
Shimada666 <649940882@qq.com>
2018-09-19 09:36:53 +0800

    Initial commit

---
+++ 070爬楼梯.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        way = [0, 1, 2]
+        for i in range(3, n + 1):
+            way.append(way[i - 1] + way[i - 2])
+        return way[n]
+
+a=Solution()
+print(a.climbStairs(5))
