8cf26ba055f9dbfed6d6696d3ee0035edc12125c
Shimada666 <649940882@qq.com>
2019-02-10 16:24:39 +0800

    Initial commit

---
+++ 122. 买卖股票的最佳时机 II.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                profit += prices[i] - prices[i - 1]
+        return profit
+
