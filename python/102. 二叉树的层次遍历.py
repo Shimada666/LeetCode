a1fc4f7710b7c259f5f8269a6c51b4dd0cc61ca9
Shimada666 <649940882@qq.com>
2020-03-05 17:51:08 +0800

    Initial commit

---
+++ python/102. 二叉树的层次遍历.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+    :author: Shimada666
+    :url: https://github.com/shimada666
+    :copyright: © 2020 Shimada666 <Shimada666@foxmail.com>
+    :license: MIT, see LICENSE for more details.
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        WHITE, GRAY = 0, 1
+        stack = []
+        init_level = 0
+        stack.append((root, WHITE, init_level))
+        result = []
+        while stack:
+            node, color, level = stack.pop()
+            if node:
+                if color == WHITE:
+                    stack.append((node.right, WHITE, level + 1))
+                    stack.append((node.left, WHITE, level + 1))
+                    stack.append((node, GRAY, level))
+                else:
+                    if len(result) == level:
+                        result.append([])
+                    result[level].append(node.val)
+        return result[::-1]
---
+++ python/107. 二叉树的层次遍历 II.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        WHITE, GRAY = 0, 1
+        stack = []
+        init_level = 0
+        stack.append((root, WHITE, init_level))
+        result = []
+        while stack:
+            node, color, level = stack.pop()
+            if node:
+                if color == WHITE:
+                    stack.append((node.right, WHITE, level + 1))
+                    stack.append((node.left, WHITE, level + 1))
+                    stack.append((node, GRAY, level))
+                else:
+                    if len(result) == level:
+                        result.append([])
+                    result[level].append(node.val)
+        return result
