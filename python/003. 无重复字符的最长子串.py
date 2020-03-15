906d535ed982a1d770cc109797bfe3db845de369
Shimada666 <649940882@qq.com>
2020-03-16 02:07:37 +0800

    Initial commit

---
+++ python/003. 无重复字符的最长子串.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+    :author: Shimada666
+    :url: https://github.com/shimada666
+    :copyright: © 2020 Shimada666 <Shimada666@foxmail.com>
+    :license: MIT, see LICENSE for more details.
+"""
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if not s: return 0
+        left = 0
+        lookup = set()
+        n = len(s)
+        max_len = 0
+        cur_len = 0
+        for i in range(n):
+            cur_len += 1
+            while s[i] in lookup:
+                lookup.remove(s[left])
+                left += 1
+                cur_len -= 1
+            if cur_len > max_len: max_len = cur_len
+            lookup.add(s[i])
+        return max_len
